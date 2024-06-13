912c3e9db1e73b31c4bb7ab148baa936031bbad6
wagner <wfbelarmino@gmail.com>
2024-06-13 18:19:50 -0300

    03 07 Refatorando o código

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ['Pizza', 'Sushi']
+
 def exibir_nome_do_programa():
     print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -17,13 +19,34 @@ def exibir_opcoes():
     print('4. Sair\n')
 
 def finalizar_app():
-    os.system('cls')
-    print ('Finalizando o app\n')
+    exibir_subtitulo('Finalizando o app')
 
+def voltar_ao_menu_principal():
+    input('Digite uma tecla para voltar ao menu principal ')
+    main() 
+    
 def opcao_invalida():
     print('Opção Inválida!\n')
-    input('Digite uma tecla para voltar ao menu principal')
-    main()
+    voltar_ao_menu_principal()   
+    
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto) 
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo ('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar:')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo('Listando restaurante')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    voltar_ao_menu_principal()
     
 def escolher_opcao():
     try:
@@ -31,9 +54,9 @@ def escolher_opcao():
         # opcao_escolhida = int(opcao_escolhida)
 
         if opcao_escolhida == 1:
-            print('Cadastrar restaurante')
+            cadastrar_novo_restaurante()
         elif opcao_escolhida == 2:
-            print('Listar restaurantes')  
+            listar_restaurantes()
         elif opcao_escolhida == 3:
             print('Ativar restaurante') 
         elif opcao_escolhida == 4:
@@ -41,8 +64,7 @@ def escolher_opcao():
         else:
             opcao_invalida() 
     except:
-        opcao_invalida()
-        
+        opcao_invalida()             
 
 def main():
     os.system('cls')
