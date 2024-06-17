85b89c334132d3e38b2efec892743c5041def8ca
wagner <wfbelarmino@gmail.com>
2024-06-17 12:02:16 -0300

    05 02 Ferramenta docstring

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ def exibir_subtitulo(texto):
     print()
 
 def cadastrar_novo_restaurante():
+    '''Essa função é responsável por cadastrar um novo restaurante'''
     exibir_subtitulo ('Cadastro de novos restaurantes')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     categoria = input(f'Digite o nome da categoria do restaurante {nome_do_restaurante}: ')
